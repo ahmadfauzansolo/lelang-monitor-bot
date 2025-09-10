bf01074bb76e78dea790296d162bf92046ffb5f7
ahmadfauzansolo <ahmadfauzansolo@gmail.com>
2025-09-11 01:02:08 +0700

    test_send.py

--- test_send.py
+++ test_send.py
@@ -1,9 +1,6 @@
 import requests
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN") or "8004194993:AAG4Dm-kJ63AIrpf_CWz8sGxF39Dm725weE"
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID") or "418103862"
-
 msg = "Halo! Ini tes dari Bot Lelang Monitor 🚀"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
